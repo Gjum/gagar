0af8312ea907e59da5ab87be2b30c1117f83198e
Gjum <gjum42@gmail.com>
2015-07-18 16:04:12 +0200

    Add ReloadableSubscriber base class

---
+++ agario_gtk/reload.py
@@ -0,0 +1,49 @@
+import importlib, sys, types
+
+
+class Reloadable(object):
+    """Makes inheriting class instances reloadable."""
+
+    _persistent_attributes = []  # will be kept when reloading
+
+    def capture_args(self, init_args):
+        """
+        Capture original args for calling init on reload.
+        An ancestor should call this in their init with locals() as argument
+        before creating any more local variables:
+
+        >>> class Foo(Reloadable):
+        >>>     def __init__(self, foo, bar=123):
+        >>>         super(self.__class__, self).__init__()
+        >>>         self.foo = foo * bar
+        >>>         self.capture_args(locals())
+        >>>         tmp = bar ** 2
+        >>>         # ...
+        """
+        self._init_args = dict(init_args)
+        for k in list(self._init_args.keys()):
+            if k == 'self' or k[:2] == '__':
+                del self._init_args[k]
+
+    def reload(self):
+        """
+        Reloads the containing module and replaces all instance attributes
+        (monkey-patching, see https://filippo.io/instance-monkey-patching-in-python/ )
+        while keeping the attributes in _persistent_attributes.
+        """
+        persistent = {k: getattr(self, k) for k in self._persistent_attributes}
+        new_module = importlib.reload(sys.modules[self.__module__])
+        new_class = getattr(new_module, self.__class__.__name__)
+        for new_attr_name in dir(new_class):
+            if new_attr_name in ('__class__', '__dict__', '__weakref__'):
+                continue  # do not copy '__dict__', '__weakref__'; copy '__class__' below
+            new_attr = getattr(new_class, new_attr_name)
+            try:  # some attributes are instance methods, bind them to `self`
+                new_attr = types.MethodType(new_attr, self)
+            except TypeError:
+                pass
+            setattr(self, new_attr_name, new_attr)
+        setattr(self, '__class__', new_class)
+        new_class.__init__(self, **getattr(self, '_init_args', {}))
+        for k, v in persistent.items():
+            setattr(self, k, v)
