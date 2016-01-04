6ba633f88a3a3a049a31d5e8d57f86f44708f404
Gjum <code.gjum@gmail.com>
2016-01-04 22:41:31 +0100

    Add version number and GitHub link to terminal greeting

--- gagar/main.py
+++ gagar/main.py
@@ -275,7 +275,9 @@ def main():
     print("Copyright (C) 2015  Gjum  <code.gjum@gmail.com>\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it\n"
-          "under certain conditions; see LICENSE.txt for details.\n")
+          "under certain conditions; see LICENSE.txt for details.\n"
+          "Project homepage: https://github.com/Gjum/gagar\n"
+          "Version: 0.1.1\n")
 
     if len(sys.argv) > 1 and sys.argv[1] in ('-h', '--help'):
         print("Usage: %s [nick]" % sys.argv[0])
